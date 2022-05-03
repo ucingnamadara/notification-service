d0e2e041c9684ac06667600811b1d8c77f89a4d1
anandapratama03 <anandapratama03@gmail.com>
2022-05-03 22:02:11 +0700

    initialize

---
+++ src/main/java/com/rumahbangsa/notification/services/NotificationService.java
@@ -0,0 +1,57 @@
+package com.rumahbangsa.notification.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
+import io.smallrye.context.impl.wrappers.ContextualRunnable;
+import io.smallrye.context.impl.wrappers.ContextualRunnableN;
+import io.smallrye.reactive.messaging.annotations.Merge;
+import org.eclipse.microprofile.context.ManagedExecutor;
+import org.eclipse.microprofile.context.ThreadContext;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+@ApplicationScoped
+public class NotificationService {
+
+    Logger LOGGER = LoggerFactory.getLogger(NotificationService.class);
+
+    @Inject
+    ManagedExecutor managedExecutor;
+
+    @Inject
+    ThreadContext threadContext;
+
+    @Inject
+    Mailer mailer;
+
+    ObjectMapper om = new ObjectMapper();
+
+    @Incoming("send-email-html")
+    @Merge(Merge.Mode.MERGE)
+    @SuppressWarnings("unchecked")
+    private CompletableFuture<Void> incomingSendEmailHtml(Message<String> message){
+        return managedExecutor.runAsync(threadContext.contextualRunnable(()->{
+            try {
+                Map<String, Object> payload = om.readValue(message.getPayload(), Map.class);
+
+                String body = (String) payload.get("body");
+                String subject = (String) payload.get("subject");
+                String to = (String) payload.get("to");
+
+                mailer.send(Mail.withHtml(to, subject, body));
+
+                message.ack();
+            } catch (Exception e){
+                LOGGER.error(e.getMessage());
+            }
+        }));
+    }
+}
---
+++ src/main/java/com/rumahbangsa/notification/utils/SimpleResponse.java
@@ -0,0 +1,37 @@
+package com.rumahbangsa.notification.utils;
+
+public class SimpleResponse {
+    public Long status;
+    public String message;
+    public Object payload;
+
+    public SimpleResponse(Long status, String message, Object payload) {
+        this.status = status;
+        this.message = message;
+        this.payload = payload;
+    }
+
+    public Long getStatus() {
+        return status;
+    }
+
+    public void setStatus(Long status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getPayload() {
+        return payload;
+    }
+
+    public void setPayload(Object payload) {
+        this.payload = payload;
+    }
+}
